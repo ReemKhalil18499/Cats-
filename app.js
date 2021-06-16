0bad0240627710eb4ecceaf93c70ef696d948a8f
ReemKhalil99 <>
2021-06-16 17:45:47 +0000

    saving

---
+++ app.js
@@ -0,0 +1,26 @@
+alert("Welcome to cats Website !")
+var cats=prompt("type pets to start the excitment !");
+ if (cats=="pets") {
+   alert("Perfect!")
+ }
+else {
+   alert("no they are not")
+
+
+ }
+
+var cats=prompt("whats your favorite type of cat ?");
+var cats=prompt("whats your Name?");
+alert("Thank you! Now u will witness cutness overload")
+
+
+
+
+
+
+
+
+
+
+
+
