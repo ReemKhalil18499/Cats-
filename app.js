7aae25fe6f32e30fd36e49d5e25c2974e600a36a
ReemKhalil99 <>
2021-06-17 13:38:25 +0000

    edited

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var cats=prompt("type pets to start the excitment !");
 else {
    alert("no they are not")
 
+ 
+}
 
- }
 var cats=prompt("whats your Name?");
 alert("Thank you! Now u will witness cutness overload")
 var cats=prompt("whats your favorite type of cat ?");
@@ -34,18 +35,17 @@ for(var r=0;r<num ; r++){
   document.write (r+'<img src="https://cattime.com/assets/uploads/gallery/maine-coon-cats-and-kittens/maine-coon-cats-and-kittens-5.jpg">') 
 }
 }
-else if (cats=="Sphynx cat
-"){
+else if (cats=="Sphynx cat"){
 var num =prompt ("how many cats you would like to see ?")
 for(var r=0;r<num ; r++){
   document.write (r+'<img src="https://www.thesprucepets.com/thmb/LHDE5LiUi-jQFZYwmjRJ9VUWl0I=/3744x3744/smart/filters:no_upscale()/sphynx-cat--159462605-5bc4cc6b46e0fb0026b71ea7.jpg">')
 }
 }
-else if (cats=="Munchkin cat
-"){
+else if (cats=="Munchkin cat"){
 var num =prompt ("how many cats you would like to see ?")
 for(var r=0;r<num ; r++){
-  document.write (r+'<img src="https://i.pinimg.com/736x/c9/1c/9e/c91c9e3ab08726ed53c1423a9c3935e8.jpg">') 
+  document.write (r+'<img src="https://i.pinimg.com/736x/c9/1c/9e/c91c9e3ab08726ed53c1423a9c3935e8.jpg">') }
+  }
 
 
 
