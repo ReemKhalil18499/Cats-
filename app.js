cd37e07271beae52ef2cb2672d799ce1960c66e9
ReemKhalil99 <>
2021-06-17 13:14:27 +0000

    new

--- app.js
+++ app.js
@@ -1,17 +1,77 @@
-alert("Welcome to cats Website !")
-var cats=prompt("type pets to start the excitment !");
- if (cats=="pets") {
-   alert("Perfect!")
- }
-else {
-   alert("no they are not")
+// alert("Welcome to cats Website !")
+// var cats=prompt("type pets to start the excitment !");
+//  if (cats=="pets") {
+//    alert("Perfect!")
+//  }
+// else {
+//    alert("no they are not")
 
 
- }
-
+//  }
+// var cats=prompt("whats your Name?");
+// alert("Thank you! Now u will witness cutness overload")
 var cats=prompt("whats your favorite type of cat ?");
-var cats=prompt("whats your Name?");
-alert("Thank you! Now u will witness cutness overload")
+var y 
+while(cats !== "Persian cat" && cats !== "British Shorthair" && cats !=="Maine Coon"&& cats !== "Sphynx cat"&& cats !== "Munchkin cat"){
+cats=prompt("whats your favorite type of cat ?");
+y++ 
+}
+if(cats=="Persian cat"){
+  var num =prompt ("how many cats you would like to see ?")
+for(var r=0;r<num ; r++){
+  document.write (r+'<img src="https://www.petmd.com/sites/default/files/styles/article_image/public/white-persian-cats-picture-id637190306.jpg?itok=UZI3pQwD">') 
+}
+}
+else if (cats=="British Shorthair"){
+var num =prompt ("how many cats you would like to see ?")
+for(var r=0;r<num ; r++){
+  document.write (r+'<img src="https://i.pinimg.com/originals/37/b5/c8/37b5c80f7078b9a94052a51185d389a5.jpg">') 
+}
+}
+else if (cats=="Maine Coon"){
+var num =prompt ("how many cats you would like to see ?")
+for(var r=0;r<num ; r++){
+  document.write (r+'<img src="https://cattime.com/assets/uploads/gallery/maine-coon-cats-and-kittens/maine-coon-cats-and-kittens-5.jpg">') 
+}
+}
+else if (cats=="Sphynx cat
+"){
+var num =prompt ("how many cats you would like to see ?")
+for(var r=0;r<num ; r++){
+  document.write (r+'<img src="https://www.thesprucepets.com/thmb/LHDE5LiUi-jQFZYwmjRJ9VUWl0I=/3744x3744/smart/filters:no_upscale()/sphynx-cat--159462605-5bc4cc6b46e0fb0026b71ea7.jpg">')
+}
+}
+else if (cats=="Munchkin cat
+"){
+var num =prompt ("how many cats you would like to see ?")
+for(var r=0;r<num ; r++){
+  document.write (r+'<img src="https://cattime.com/assets/uploads/gallery/maine-coon-cats-and-kittens/maine-coon-cats-and-kittens-5.jpg">') 
+
+// var nuvm =prompt("How many times you would like to see the image ?")
+// if(cats == "Persian cat"){
+// for(var r=1;r <=num ;r++){
+// document.write(r+"<div>"+"h3"+"Persian cat"+"</h3>"+"</div>"+ "https://www.petmd.com/sites/default/files/styles/article_image/public/white-persian-cats-picture-id637190306.jpg?itok=UZI3pQwD ") 
+// }
+
+// }
+// else if (cats == "British Shorthair")
+// for(var r-0;r <num ;)
+// {document.write(r+"<div>"+"h3"+"British Shorthair"+"</h3>"+"<img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Maine_Coon_cat_by_Tomitheos.JPG" alt="cats">  alt="cats""> 
+// }
+// else if (cats == "Maine Coon")
+
+
+
+
+
+
+
+// }
+
+
+
+
+
 
 
 
