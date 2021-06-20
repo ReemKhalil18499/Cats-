913664136948e60abf50487315ec941c153f5e3c
ReemKhalil99 <>
2021-06-20 10:48:02 +0000

    new

--- app.js
+++ app.js
@@ -1,75 +1,85 @@
-alert("Welcome to cats Website !")
-var cats=prompt("type pets to start the excitment !");
- if (cats=="pets") {
-   alert("Perfect!")
- }
-else {
-   alert("no they are not")
-
-
- }
-var cats=prompt("whats your Name?");
-alert("Thank you! Now u will witness cutness overload")
-var cats=prompt("whats your favorite type of cat ?");
-var y 
-while(cats !== "Persian cat" && cats !== "British Shorthair" && cats !=="Maine Coon"&& cats !== "Sphynx cat"&& cats !== "Munchkin cat"){
-cats=prompt("whats your favorite type of cat ?");
-y++ 
-}
-if(cats=="Persian cat"){
-  var num =prompt ("how many cats you would like to see ?")
-for(var r=0;r<num ; r++){
-  document.write (r+'<img src="https://www.petmd.com/sites/default/files/styles/article_image/public/white-persian-cats-picture-id637190306.jpg?itok=UZI3pQwD">') 
-}
-}
-else if (cats=="British Shorthair"){
-var num =prompt ("how many cats you would like to see ?")
-for(var r=0;r<num ; r++){
-  document.write (r+'<img src="https://i.pinimg.com/originals/37/b5/c8/37b5c80f7078b9a94052a51185d389a5.jpg">') 
-}
-}
-else if (cats=="Maine Coon"){
-var num =prompt ("how many cats you would like to see ?")
-for(var r=0;r<num ; r++){
-  document.write (r+'<img src="https://cattime.com/assets/uploads/gallery/maine-coon-cats-and-kittens/maine-coon-cats-and-kittens-5.jpg">') 
-}
-}
-else if (cats=="Sphynx cat
-"){
-var num =prompt ("how many cats you would like to see ?")
-for(var r=0;r<num ; r++){
-  document.write (r+'<img src="https://www.thesprucepets.com/thmb/LHDE5LiUi-jQFZYwmjRJ9VUWl0I=/3744x3744/smart/filters:no_upscale()/sphynx-cat--159462605-5bc4cc6b46e0fb0026b71ea7.jpg">')
-}
-}
-else if (cats=="Munchkin cat
-"){
-var num =prompt ("how many cats you would like to see ?")
-for(var r=0;r<num ; r++){
-  document.write (r+'<img src="https://i.pinimg.com/736x/c9/1c/9e/c91c9e3ab08726ed53c1423a9c3935e8.jpg">') 
-
-
-
-
-
-
-
+// alert("Welcome to cats Website !")
+// var cats=prompt("type pets to start the excitment !");
+//  if (cats=="pets") {
+//    alert("Perfect!")
+//  }
+// else {
+//    alert("no they are not")
 
 
 // }
 
+// var cats=prompt("whats your Name?");
+// alert("Thank you! Now u will witness cutness overload")
+
+
+// cats=prompt("whats your favorite type of cat ?");
+// y++ 
+// }
+// }
+// if(cats=="Persian cat"){
+//   var num =prompt ("how many cats you would like to see ?")
+// for(var r=0;r<num ; r++){
+//   document.write (r+'<img src="https://www.petmd.com/sites/default/files/styles/article_image/public/white-persian-cats-picture-id637190306.jpg?itok=UZI3pQwD">') 
+// }
+// }
+// else if (cats=="British Shorthair"){
+// var num =prompt ("how many cats you would like to see ?")
+// for(var r=0;r<num ; r++){
+//   document.write (r+'<img src="https://i.pinimg.com/originals/37/b5/c8/37b5c80f7078b9a94052a51185d389a5.jpg">') 
+// }
+// }
+// else if (cats=="Maine Coon"){
+// var num =prompt ("how many cats you would like to see ?")
+// for(var r=0;r<num ; r++){
+//   document.write (r+'<img src="https://cattime.com/assets/uploads/gallery/maine-coon-cats-and-kittens/maine-coon-cats-and-kittens-5.jpg">') 
+// }
+// }
+// else if (cats=="Sphynx cat"){
+// var num =prompt ("how many cats you would like to see ?")
+// for(var r=0;r<num ; r++){
+//   document.write (r+'<img src="https://www.thesprucepets.com/thmb/LHDE5LiUi-jQFZYwmjRJ9VUWl0I=/3744x3744/smart/filters:no_upscale()/sphynx-cat--159462605-5bc4cc6b46e0fb0026b71ea7.jpg">')
+// }
+// }
+// else if (cats=="Munchkin cat"){
+// var num =prompt ("how many cats you would like to see ?")
+// for(var r=0;r<num ; r++){
+//   document.write (r+'<img src="https://i.pinimg.com/736x/c9/1c/9e/c91c9e3ab08726ed53c1423a9c3935e8.jpg">') }
+//   
 
 
 
+// var num = prompt("how many family members u have?");
+// // while(reem != "3" && reem !="4")
 
 
+// if (num == "3") {
+//   var names = prompt("dana + reem + bader")
+//   if (names == "reem") {
+//     document.write("<img src='https://c.files.bbci.co.uk/957C/production/_111686283_pic1.png'>")
+//   }
+//   else if (names == "dana") {
+//     document.write("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/2010-kodiak-bear-1.jpg/1200px-2010-kodiak-bear-1.jpg'>")
+//   }
 
+// }
+function cats() {
+  var cats = prompt("whats your favorite type of cat ?");
+  var y
+  while (cats !== "Persian cat" && cats !== "British Shorthair" && cats !== "Maine Coon" && cats !== "Sphynx cat" && cats !== "Munchkin cat") {
+    cats = prompt("you entered a type that we dont have sorry")
+  }
+}
+cats();
 
+function meow() {
+  var meow = prompt("How many cats you will have now ?")
 
+  return parseInt(meow);
+}
+var cat = meow();
+for (var i = 0; i < cat; i++) {
+  document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSk76WJjPf3jPQdpirOUt7Zf9MuwEbz2-0SQ&usqp=CAU">')
 
-
-
-
-
-
-
+}
 
